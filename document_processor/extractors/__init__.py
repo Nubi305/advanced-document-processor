02ea8839d89392d1ab4ff216b5f01f21aaf32023
Nubi305 <rodnyfernandez001@gmail.com>
2025-05-09 17:51:47 -0400

    Add extractors package initialization

---
+++ document_processor/extractors/__init__.py
@@ -0,0 +1,13 @@
+from .text_extractor import TextExtractor
+from .table_extractor import TableExtractor
+from .ocr_extractor import OCRExtractor
+from .layout_extractor import LayoutExtractor
+from .financial_extractor import FinancialExtractor
+
+__all__ = [
+    'TextExtractor',
+    'TableExtractor',
+    'OCRExtractor',
+    'LayoutExtractor',
+    'FinancialExtractor'
+]
